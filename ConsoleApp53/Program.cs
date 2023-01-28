e633388ec300397b1062950fcbee323708f0481b
naimar1996 <naimar@code.edu.az>
2023-01-28 23:24:46 +0400

    Method

---
+++ ConsoleApp53/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleApp53
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(FindavailableNumber(number));
+
+
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(Findsumtillnumber(number));
+
+            Console.WriteLine("----");
+            double number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----");
+            double number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter symbols(>)");
+            string comparison = Console.ReadLine();
+            switch (comparison)
+            {
+                case ">":
+                    int bigger;
+                    Console.WriteLine(number1 > number2);
+                    break;
+
+            }
+
+
+    }
+        //static int FindavailableNumber (int number)
+        //{
+        //    if(number % 7 == 0)
+        //    {
+        //        return number;
+        //    }
+        //    while (number % 7 != 0)
+        //    {
+        //        if (number % 7 > 3)
+        //        {
+        //            number++;
+        //        }
+        //        else
+        //        {
+        //            number--;
+        //        }
+        //    }
+        //    return number; 
+        //}
+
+
+        //static int Findsumtillnumber(int s)
+        //{
+        //    int sum = 0; 
+        //    for ( int n = 0; 0 <= s; n++)
+        //    {
+        //        sum += n;
+        //    }
+        //    return sum;
+        //}
+
+
+      
+            
+
+    }
